e5b83620466dd0ed5a1fb72bc868a2cc686085c6
HajinKimm <11rlagkwls@naver.com>
2023-10-09 02:47:36 +0900

    HashRouter CHANGE

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import {  BrowserRouter ,  Routes,  Route,  Link  } from "react-router-dom";
+import {  HashRouter ,  Routes,  Route,  Link  } from "react-router-dom";
 import GlobalStyle from './styled/Global';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 const App = () => {
   return (
-    <BrowserRouter>
-      <GlobalStyle/>
+    <>
+    <GlobalStyle/>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<Layout/>}>
           <Route index element={<Home/>}/>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
+    </>
   );
 };
 
